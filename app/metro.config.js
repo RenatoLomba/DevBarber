dc5df850516a0c249dd06e12c7eb4ed89e5e7700
RenatoLomba <63821245+RenatoLomba@users.noreply.github.com>
2021-01-20 16:53:23 -0300

    update projeto

--- app/metro.config.js
+++ app/metro.config.js
@@ -1,30 +1,18 @@
-/* eslint-disable prettier/prettier */
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
-const { getDefaultConfig } = require('@expo/metro-config');
+const { getDefaultConfig } = require("@expo/metro-config");
 
 module.exports = (async () => {
   const {
-    resolver: { sourceExts, assetExts },
+    resolver: { sourceExts, assetExts }
   } = await getDefaultConfig(__dirname);
   return {
     transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: false,
-        },
-      }),
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      assetPlugins: ['expo-asset/tools/hashAssetFiles'],
+      babelTransformerPath: require.resolve("react-native-svg-transformer")
     },
     resolver: {
-      assetExts: assetExts.filter((ext) => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
-    },
+      assetExts: assetExts.filter(ext => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"]
+    }
   };
 })();
